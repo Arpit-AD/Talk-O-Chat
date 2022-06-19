2e9d2ad54c6c67c4bd8427991387fc937d6532cd
Arpit-AD <arpit2001.ad@gmail.com>
2022-06-19 19:05:35 +0100

    Frontend

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/api', (req, res) => {
   res.send("Server is up and running");
 });
 
